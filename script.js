b2627f2a36a68396224201de7ee5933720fa3d4a
ALDO LAMBRUSCHINI <adloans@gmail.com>
2015-10-23 20:07:24 -0700

    STG

--- script.js
+++ script.js
@@ -6,25 +6,59 @@
  * student_array - global array to hold student objects
  * @type {Array}
  */
-    studentArray = [];
+
+//array to hold student info objects.
+
+
+   var studentArray = [];
+console.log(studentArray);
 /**
  * inputIds - id's of the elements that are used to add students
  * @type {string[]}
+ *
  */
-    inputIds =  [{studentName: ""}, {studentCourse: ""}, {studentGrade: ""}];
+      //global variable to get value of input data.
+
+   var student = {
+   studentName: $("#studentName").val(),
+   studentCourse: $("#studentCourse").val(),
+   studentGrade: $("#studentGrade").val()
+};
+console.log(student);
+
+   //user submit button(stores date from student object to student array
 /**
+ *
  * addClicked - Event Handler when user clicks the add button
  */
+function addClicked(){
+}
 
 /**
  * cancelClicked - Event Handler when user clicks the cancel button, should clear out student form
  */
 
+//clear student form
+
+function cancelClicked(){
+
+}
+
+/// creates student objects and stores in studentArray
 /**
  * addStudent - creates a student objects based on input fields in the form and adds the object to global student array
- *
+ *a
  * @return undefined
  */
+  $("input").change(function() {
+      var value = $(this).val();
+      student.value = value;
+      studentArray.push(student);
+      console.log('my object is: ', student);
+      console.log('student array now holds ', studentArray);
+
+   });
+
 
 /**
  * clearAddStudentForm - clears out the form values based on inputIds variable
