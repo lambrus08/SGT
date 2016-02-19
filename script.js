f86a6ac14abb909fb885d423c8b949d118d15afd
aldo lambruschini <adloans@gmail.com>
2016-02-19 13:54:44 -0800

    working on delete button, most delete current row pressed

--- script.js
+++ script.js
@@ -19,11 +19,57 @@ var SchoolTemplate = function () {
         var student = new StudentTemplate();
         student.assign(name, course, grade);
         this.studentArray.push(student);
+        self.calculateAverage();
+        self.addStudentToDom(student);
+        self.clearValuesFromForm();
         console.log('current status of student array:', this.studentArray);
+    };
+    self.calculateAverage = function(){
+        var sum = 0;
+        for(i = 0; i < this.studentArray.length; i++){
+            sum += parseInt(this.studentArray[i]['grade']);
+            console.log('sum of all grades =', sum);
+        }
+        var average = sum/this.studentArray.length;
+        console.log('This is my average:', average);
+        $('.avgGrade').text(average.toFixed(2));
+        return average;
+
+    };
+
+    self.addStudentToDom = function(student) {
+        console.log('add student to dom check: ', student.name);
+        var td1 = $('<td>').text(student.name);
+        var td2 = $('<td>').text(student.course);
+        var td3 = $('<td>').text(student.grade);
+        var td4 = $('<td>');
+        var button = $('<button>').addClass('btn btn-danger').text('Delete').attr('type', 'button');
+        var tr = $('<tr>');
+        td4.append(button);
+        $('tbody').append(tr);
+        $(tr).append(td1, td2, td3, td4);
+        button.click(function(){
+            var element = this;
+            $(element).empty();
+            console.log('delete button clicked');
+
+        })
+    };
+    self.clearValuesFromForm = function(){
+        $('#studentName').val('');
+        $('#studentCourse').val('');
+        $('#studentGrade').val('');
+    };
+    self.cancelClicked = function(){
+        self.clearValuesFromForm();
+        this.studentArray = [];
+        $('tbody').empty();
+        $('.avgGrade').text('0');
+
     }
-
-
 };
+
+
 
 var StudentTemplate = function(){
     var self = this;
